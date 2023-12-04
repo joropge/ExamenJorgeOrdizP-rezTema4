e432bb91b5d43b3041d6ef3159ea315850bcd083
joropge <jorgeop1997@gmail.com>
2023-12-04 08:57:34 +0100

    creados los getter y setter de la clase vehiculo

--- scripts/vehiculo.js
+++ scripts/vehiculo.js
@@ -1,4 +1,3 @@
-//Crea propiedades privadas para la marca, modelo, color, año de fabricación y cilindrada del vehículo.
 class Vehiculo{
     #marca;
     #modelo;
@@ -12,4 +11,52 @@ class Vehiculo{
         this.#añoFabricación = añoFabricación;
         this.#cilindrada = cilindrada;
     }
+    get marca(){
+        return this.#marca;
+    }
+    set marca(marca){
+        this.#marca = marca;
+    }
+    get modelo(){
+        return this.#modelo;
+    }
+    set modelo(modelo){
+        this.#modelo = modelo;
+    }
+    get color(){
+        return this.#color;
+    }
+    set color(color){
+        this.#color = color;
+    }
+    get añoFabricación(){
+        return this.#añoFabricación;
+    }
+    set añoFabricación(añoFabricación){
+        this.#añoFabricación = añoFabricación;
+    }
+    get cilindrada(){
+        return this.#cilindrada;
+    }
+    set cilindrada(cilindrada){
+        this.#cilindrada = cilindrada;
+    }
+
+    mostrarDatos(){
+    }
 }
+
+/*•	Crea propiedades privadas para la marca, modelo, color, año de fabricación y cilindrada del vehículo.
+
+•	Implementa un constructor que tome como parámetros la marca, modelo, color, año de fabricación y cilindrada, y asigna estos valores a las propiedades privadas del vehículo.
+
+•	Método mostrarDatos: Desarrolla un método llamado mostrarDatos que genere y devuelva una cadena de texto con la información del vehículo. Incluye la marca, modelo, color, año de fabricación y cilindrada.
+
+•	Método acelerar: Implementa el método acelerar, que tome como parámetro la velocidad y muestre un mensaje indicando que el vehículo ha acelerado hasta la velocidad proporcionada.
+
+•	Método arrancar y frenar: Crea métodos llamados arrancar y frenar. Ambos métodos deben mostrar mensajes indicando que el vehículo ha arrancado o frenado, respectivamente.
+
+•	Métodos Getter y Setter: Define métodos getter y setter para cada una de las propiedades privadas del vehículo (marca, modelo, color, año de fabricación, cilindrada).
+
+•	Uso de mostrarResultado: Utiliza la función mostrarResultado (definida en main.js) en los métodos mostrarDatos, acelerar, arrancar y frenar para mostrar los resultados en la interfaz.
+ */
